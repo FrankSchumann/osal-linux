d53a2c38e2cde759e2d6ef9fd1846df909b7e198
Frank Schumann <frank.schumann.76@gmail.com>
2022-12-26 07:39:34 +0100

    initial project

---
+++ osal-linux.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "config.h"
+
+int main(int argc, char **argv) {
+	std::cout << "Hello World" << std::endl;
+//	std::cout << "Version " << osal-linux_VERSION_MAJOR << "." << osal-linux_VERSION_MINOR << std::endl;
+	return 0;
+}
