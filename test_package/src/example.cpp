5a18a2cdbdd07731e84b6b64b09a07e5502846b0
Frank Schumann <frank.schumann.76@gmail.com>
2023-01-27 00:53:32 +0100

    Add checks into test_package for the other headers and some symbols from Semaphore etc.

--- test_package/src/example.cpp
+++ test_package/src/example.cpp
@@ -1,5 +1,14 @@
 #include "osal.h"
 
+#include "osalinfo/OsalInfoIf.h"
+#include "osalinfo/OsalInfoFactory.h"
+
+#include "semaphore/SemaphoreFactory.h"
+#include "semaphore/SemaphoreIf.h"
+
 int main() {
     osal();
+
+    COPA::OsalInfoFactory osalInfoFactory;
+    COPA::SemaphoreFactory semaphoreFactory;
 }
