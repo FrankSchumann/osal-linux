d54127acb83b1bb75a3935e1481ca728b347d2ab
Frank Schumann <frank.schumann.76@gmail.com>
2023-01-30 23:31:01 +0100

    install header to include/osal

--- test_package/src/example.cpp
+++ test_package/src/example.cpp
@@ -1,10 +1,11 @@
-#include "Info.h"
-#include "semaphore/SemaphoreFactory.h"
-#include "semaphore/SemaphoreIf.h"
+#include "osal/Info.h"
+#include "osal/semaphore/SemaphoreFactory.h"
+#include "osal/semaphore/SemaphoreIf.h"
 
 int main()
 {
     osal::Info::show();
+
 
     osal::SemaphoreFactory semaphoreFactory;
 }
